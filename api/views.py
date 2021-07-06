6029d870a9cdd901d0728da4aaf28115529cfa8c
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-06 12:53:39 +0700

    Added authentication and permission classes to Generic API

--- api/views.py
+++ api/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
+from rest_framework import serializers, status, generics, mixins
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.decorators import api_view
 from rest_framework.parsers import JSONParser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework import serializers, status, generics, mixins
 from rest_framework.views import APIView
+
 
 from .models import Article
 from .serializers import ArticleSerializer
@@ -22,8 +25,9 @@ class GenericAPIView(
     
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-
     lookup_field = 'id'
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request, id=None):
         if id:
@@ -37,7 +41,7 @@ class GenericAPIView(
     def put(self, request, id=None):
         return self.update(request, id)
 
-    def delete(self, request, id=None):
+    def delete(self, request, id):
         return self.destroy(request, id)
 
 
