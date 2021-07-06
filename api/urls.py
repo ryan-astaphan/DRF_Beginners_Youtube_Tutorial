72eb1d9297c92319629a772fc1b1215ebdc32523
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-06 12:08:15 +0700

    Created a class-based view (APIView) and learned about DRF's Response class.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import article_list, article_detail
+from .views import article_list, article_detail, ArticleAPIView, ArticleDetail
 
 urlpatterns = [
-    path('articles/', article_list, name='article_list'),
-    path('articles/<int:pk>/', article_detail, name='article_detail'),
+    # path('articles/', article_list, name='article_list'),
+    # path('articles/<int:pk>/', article_detail, name='article_detail'),
+    path('articles/', ArticleAPIView.as_view(), name='article_list'),
+    path('articles/<int:id>/', ArticleDetail.as_view(), name='article_detail'),
+    
 ]
