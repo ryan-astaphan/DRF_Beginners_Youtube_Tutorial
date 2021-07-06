04cdf021f96eee67114e31edfbafb46ed19b1417
ryan-in-thailand <ryanastaphan@gmail.com>
2021-07-06 13:52:35 +0700

    Switched from viewset.ViewSet to viewset.GenericViewSet. The Generic viewset, when coupled with mixins,  provides lots of functionality with very little code!

--- api/views.py
+++ api/views.py
@@ -14,33 +14,16 @@ from .models import Article
 from .serializers import ArticleSerializer
 
 # Create your views here.
-class ArticleViewSet(viewsets.ViewSet):
-    def list(self, request):
-        queryset = Article.objects.all()
-        serializer = ArticleSerializer(queryset, many=True)
-        return Response(serializer.data)
-    
-    def create(self, request):
-        serializer = ArticleSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def retrieve(self, request, pk=None):
-        queryset = Article.objects.all()
-        article = get_object_or_404(queryset, pk=pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-
-    def update(self, request, pk=None):
-        article = Article.objects.get(pk=pk)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArticleViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+    ):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
 
 
 class GenericAPIView(
